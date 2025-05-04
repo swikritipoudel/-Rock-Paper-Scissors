7038badde7ed93e20dcfbf6ec53c91803146084c
Swikriti Poudel <poudelswikriti76@gmail.com>
2025-05-04 21:50:34 +0545

    Added html, css and js file Did some styling and added functionality to display player choice, computer choice and also the result .

---
+++ script.js
@@ -0,0 +1,52 @@
+let playerDisplay = document.getElementById("playerDisplay")
+let computerDisplay = document.getElementById("computerDisplay")
+let resultDisplay = document.getElementById("resultDisplay")
+let choices = ["rock", "paper", "scissors"]
+
+function playGame(playerChoice){
+    
+
+    let computerChoice = choices[Math.floor(Math.random()*choices.length)]
+
+    let result= "";
+
+    if(playerChoice=== computerChoice){
+        result = "IT'S A TIE"
+    }
+
+    else{
+        switch(playerChoice){
+            case "rock" :
+            result = (computerChoice==="scissors") ? "YOU WIN" : "YOU LOSE"
+            break;
+
+            case "paper" :
+            result = (computerChoice==="rock") ? "YOU WIN" : "YOU LOSE"
+            break;
+
+            case "scissors" :
+            result = (computerChoice==="paper") ? "YOU WIN" : "YOU LOSE"
+            break;
+        }
+    }
+
+    playerDisplay.textContent = `Player: ${playerChoice}`
+    computerDisplay.textContent = `Computer: ${computerChoice}`
+    resultDisplay.textContent = `${result}`
+    
+    resultDisplay.classList.remove("greenText", "redText")
+
+    switch(result){
+        case "YOU WIN":
+        resultDisplay.classList.add("greenText")
+        break 
+        
+        case "YOU LOSE":
+        resultDisplay.classList.add("redText") 
+        break   
+    }
+}
+
+
+
+
